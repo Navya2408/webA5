ffbe1b0ae891db3fb91089cd1bed51269d3dbe84
Navya <navya.0248@gmail.com>
2024-04-16 19:11:07 -0400

    initial commit

--- modules/unCountries.js
+++ modules/unCountries.js
@@ -1,7 +1,66 @@
+require("dotenv").config();
+const Sequelize = require("sequelize");
+
+// set up sequelize to point to our postgres database
+const sequelize = new Sequelize(
+  process.env.DB_DATABASE,
+  process.env.DB_USER,
+  process.env.DB_PASSWORD,
+  {
+    host: process.env.DB_HOST,
+    dialect: "postgres",
+    port: 5432,
+    dialectOptions: {
+      ssl: { rejectUnauthorized: false },
+    },
+  }
+);
 const countryData = require("../data/countryData");
 const regionData = require("../data/regionData");
-
 let countries = [];
+
+const Region = sequelize.define(
+  "Region",
+  {
+    id: {
+      type: Sequelize.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    name: Sequelize.STRING,
+    subs: Sequelize.STRING,
+  },
+  {
+    createdAt: false, // disable createdAt
+    updatedAt: false, // disable updatedAt
+  }
+);
+
+const Country = sequelize.define(
+  "Country",
+  {
+    a2code: {
+      type: Sequelize.STRING,
+      primaryKey: true,
+    },
+    name: Sequelize.STRING,
+    official: Sequelize.STRING,
+    nativeName: Sequelize.STRING,
+    permanentUNSC: Sequelize.BOOLEAN,
+    wikipediaURL: Sequelize.STRING,
+    capital: Sequelize.STRING,
+    regionId: Sequelize.INTEGER,
+    languages: Sequelize.STRING,
+    population: Sequelize.INTEGER,
+    flag: Sequelize.STRING,
+  },
+  {
+    createdAt: false, // disable createdAt
+    updatedAt: false, // disable updatedAt
+  }
+);
+
+Country.belongsTo(Region, { foreignKey: "regionId" });
 
 function initialize() {
   return new Promise((resolve, reject) => {
@@ -53,3 +112,30 @@ function getCountriesByRegion(region) {
 module.exports = { initialize, getAllCountries, getCountryByCode, getCountriesByRegion }
 
 
+
+sequelize
+  .sync()
+  .then( async () => {
+    try{
+      await Region.bulkCreate(regionData);
+      await Country.bulkCreate(countryData); 
+      console.log("-----");
+      console.log("data inserted successfully");
+    }catch(err){
+      console.log("-----");
+      console.log(err.message);
+
+      // NOTE: If you receive the error:
+
+      // insert or update on table "Countries" violates foreign key constraint "Countries_region_id_fkey"
+
+      // it is because you have a "country" in your collection that has a "regionId" that does not exist in the "regionData".   
+
+      // To fix this, use PgAdmin to delete the newly created "Regions" and "Countries" tables, fix the error in your .json files and re-run this code
+    }
+
+    process.exit();
+  })
+  .catch((err) => {
+    console.log('Unable to connect to the database:', err);
+  })
--- server.js
+++ server.js
@@ -1,35 +1,61 @@
 const unCountryData = require("./modules/unCountries");
+const path = require("path");
 
 const express = require('express');
 const app = express();
 
 const HTTP_PORT = process.env.PORT || 8080;
 
+app.use(express.static('public')); 
+app.set('view engine', 'ejs');
+
+
 app.get('/', (req, res) => {
-    res.send('Assignment 2:  Student Name - Student Id');
+  res.render("home")
+});
+
+app.get('/about', (req, res) => {
+  res.render("about")
 });
 
 app.get("/un/countries", async (req,res)=>{
-  let countries = await unCountryData.getAllCountries();
-  res.send(countries);
+  
+  let countries = [];
+
+  try{
+    if(req.query.region){
+      countries = await unCountryData.getCountriesByRegion(req.query.region);
+    }else{
+      countries = await unCountryData.getAllCountries();
+    }
+    // res.json(countries)
+    res.render("countries", {countries})
+  }catch(err){
+    res.status(404).render("404", {message: err});
+  }
+
 });
 
-app.get("/un/countries/country-demo", async (req,res)=>{
+app.get("/un/countries/:code", async (req,res)=>{
   try{
-    let country = await unCountryData.getCountryByCode("ca");
-    res.send(country);
+    let country = await unCountryData.getCountryByCode(req.params.code);
+    res.render("country", {country})
   }catch(err){
-    res.send(err);
+    res.status(404).render("404", {message: err});
   }
 });
 
-app.get("/un/countries/region-demo", async (req,res)=>{
-  try{
-    let countries = await unCountryData.getCountriesByRegion("Oceania");
-    res.send(countries);
-  }catch(err){
-    res.send(err);
-  }
+// app.get("/un/countries/region-demo", async (req,res)=>{
+//   try{
+//     let countries = await unCountryData.getCountriesByRegion("Oceania");
+//     res.send(countries);
+//   }catch(err){
+//     res.send(err);
+//   }
+// });
+
+app.use((req, res, next) => {
+  res.status(404).render("404", {message: "I'm sorry, we're unable to find what you're looking for"});
 });
 
 unCountryData.initialize().then(()=>{
